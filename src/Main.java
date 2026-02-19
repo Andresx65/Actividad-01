7b598b99a6498bc3410752740d5aae6939cde832
AndresG <andrespersonal2022@gmail.com>
2026-02-19 13:40:25 -0500

    Actulizacion 02

--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+System.out.println("Andres_Felipe_Guerrero_Gomez");
+System.out.println("Hobbie 1 Tocar la Guitarra, Hobbie 2 Jugar videojuegos");
+System.out.println("No, no practico ningun deporte");
